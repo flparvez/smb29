0c8964aaae69552b9d7d2119511becb483b67bc9
flparvez <flparvez23@gmail.com>
2025-03-15 15:52:19 +0600

    packages add

--- src/app/user/plans/page.tsx
+++ src/app/user/plans/page.tsx
@@ -1,11 +1,24 @@
+import Packages from '@/components/Packages'
 import Link from 'next/link'
 import React from 'react'
 
 const Plans = () => {
   return (
-    <div>
-            <Link className='text-[#7baa1b] text-xl  font-extrabold ml-6 mt-4' href="/user/dashboard">Go Dashboard</Link>
-      <h2>Plan Page</h2>
+    <div className="min-h-screen bg-[#eee] pb-10">
+    <Link href="/user/dashboard" className="h-14 w-full bg-[#0c0ce8] pl-4  gap-5 shadow text-white flex items-center "><svg
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+            className="h-7 font-bold cursor-pointer w-7">
+            <path fillRule="evenodd"
+                d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z"
+                clipRule="evenodd"></path>
+        </svg>
+        </Link>
+        <h2 className="text-xl font-bold">Packages </h2>
+<div >
+
+<Packages />
+</div>
+
     </div>
   )
 }
---
+++ src/components/Packages.tsx
@@ -0,0 +1,107 @@
+
+import React from 'react'
+
+const Packages = () => {
+
+    const packages = [
+        {
+            title: "Basic",
+            price: 500,
+            dailyAds: 3,
+            dailyIncome: 300,
+            validity: 130
+        },
+        {
+            title: "Standard",
+            price: 1000,
+            dailyAds: 5,
+            dailyIncome: 500,
+            validity: 130
+        },
+        {
+            title: "Premium",
+            price: 1500,
+            dailyAds: 7,
+            dailyIncome: 700,
+            validity: 130
+        },
+        {
+            title: "Platinum",
+            price: 2000,
+            dailyAds: 10,
+            dailyIncome: 1000,
+            validity: 210
+        },
+        {
+            title: "Gold",
+            price: 5000,
+            dailyAds: 30,       
+            dailyIncome: 3000,
+            validity: 290
+        },
+        {
+            title: "Diamond",
+            price: 10000,
+            dailyAds: 60,
+            dailyIncome: 6000,
+            validity: 330
+        }
+    ]
+  return (
+    <div className="grid grid-cols-2 gap-5 p-5 ">
+
+{
+    packages.map((item, index) => (
+        <div key={index} className=" p-5 relative bg-white w-full overflow-hidden border border-gray-300 rounded-lg">
+<div className=" bg-white">
+    <h2 className="text-xl text-center font-bold whitespace-nowrap">{item.title}</h2>
+    <p
+        className="w-7 text-black h-7 bg-none flex justify-center items-center rounded-full absolute top-0 right-0">
+        3x</p>
+    <h2 className="text-lg text-center font-bold whitespace-nowrap">{item.price}</h2>
+    <hr className="my-2" />
+    <div className="text-sm text-gray-700">
+        <p className="flex gap-2 items-center"><svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                version="1" viewBox="0 0 48 48" enable-background="new 0 0 48 48" height="1em"
+                width="1em" xmlns="http://www.w3.org/2000/svg">
+                <polygon fill="#8BC34A"
+                    points="24,3 28.7,6.6 34.5,5.8 36.7,11.3 42.2,13.5 41.4,19.3 45,24 41.4,28.7 42.2,34.5 36.7,36.7 34.5,42.2 28.7,41.4 24,45 19.3,41.4 13.5,42.2 11.3,36.7 5.8,34.5 6.6,28.7 3,24 6.6,19.3 5.8,13.5 11.3,11.3 13.5,5.8 19.3,6.6">
+                </polygon>
+                <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4">
+                </polygon>
+            </svg> Daily {item.dailyAds} Ads </p>
+        <p className="flex gap-2 items-center"><svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                version="1" viewBox="0 0 48 48" enable-background="new 0 0 48 48" height="1em"
+                width="1em" xmlns="http://www.w3.org/2000/svg">
+                <polygon fill="#8BC34A"
+                    points="24,3 28.7,6.6 34.5,5.8 36.7,11.3 42.2,13.5 41.4,19.3 45,24 41.4,28.7 42.2,34.5 36.7,36.7 34.5,42.2 28.7,41.4 24,45 19.3,41.4 13.5,42.2 11.3,36.7 5.8,34.5 6.6,28.7 3,24 6.6,19.3 5.8,13.5 11.3,11.3 13.5,5.8 19.3,6.6">
+                </polygon>
+                <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4">
+                </polygon>
+            </svg> Daily Income {item.dailyIncome} Taka </p>
+        <p className="flex gap-2 items-center"><svg stroke="currentColor" fill="currentColor" stroke-width="0"
+                version="1" viewBox="0 0 48 48" enable-background="new 0 0 48 48" height="1em"
+                width="1em" xmlns="http://www.w3.org/2000/svg">
+                <polygon fill="#8BC34A"
+                    points="24,3 28.7,6.6 34.5,5.8 36.7,11.3 42.2,13.5 41.4,19.3 45,24 41.4,28.7 42.2,34.5 36.7,36.7 34.5,42.2 28.7,41.4 24,45 19.3,41.4 13.5,42.2 11.3,36.7 5.8,34.5 6.6,28.7 3,24 6.6,19.3 5.8,13.5 11.3,11.3 13.5,5.8 19.3,6.6">
+                </polygon>
+                <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4">
+                </polygon>
+            </svg> Validity 130 days </p>
+    </div>
+                    <button  data-id="86"  data-title="Basic"  data-price="500 BDT"
+        className="buyBtn align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 shadow-blue-500/20 hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none block w-full bg-[#0f9b0f] text-white mt-4 border shadow-none rounded-lg"
+        type="button"> Buy now </button>
+                </div>
+</div>
+    ))
+}
+  
+
+ 
+
+    </div>
+  )
+}
+
+export default Packages
